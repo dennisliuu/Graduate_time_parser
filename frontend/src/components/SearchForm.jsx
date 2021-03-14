791d076f8db505b7a23fbf41911db479d3a7672b
dennisliuu <dennisliuu@gmail.com>
2021-03-14 17:37:07 +0800

    Update query method

--- frontend/src/components/SearchForm.jsx
+++ frontend/src/components/SearchForm.jsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react";
 import ShowResult from "./ShowResult";
-import { useQuery } from "@apollo/client";
 import { useLazyQuery } from "@apollo/client";
 import { LOAD_USERS } from "../GraphQL/Queries";
 
 const SearchForm = ({ schools }) => {
   const [professor, setProfessor] = useState([]);
+  const [search, setSearch] = useState({
+    Name: "",
+    School: "",
+  });
   const [executeSearch, { data }] = useLazyQuery(LOAD_USERS);
 
   useEffect(() => {
     if (data) {
       setProfessor(
         data.getAllProfessors.filter(
-          (professor) => professor.Name == "XXX" && professor.School == "NTHU"
+          (professor) =>
+            professor.Name == search.Name || professor.School == search.School
         )
       );
     }
@@ -22,7 +26,14 @@ const SearchForm = ({ schools }) => {
   schools.map((school) => {
     lists.push(
       <div className="box" key={school}>
-        <input className="input" id={school} type="radio" name="school_radio" />
+        <input
+          className="input"
+          id={school}
+          type="radio"
+          name="school_radio"
+          value={school}
+          onChange={(e) => setSearch({ School: e.currentTarget.value })}
+        />
         <label className="label is-middle" htmlFor={school}>
           <span className="radio is-margin-right-xxs"></span>
           <span className="text">{school}</span>
@@ -39,12 +50,16 @@ const SearchForm = ({ schools }) => {
         type="text"
         name="name"
         placeholder="教授名稱"
+        value={search.Name}
+        onChange={(e) => setSearch({ Name: e.target.value })}
       />
       <button
         className="button is-plain is-primary is-sm is-margin-top-xxl"
         type="button"
         onClick={
-          () => executeSearch()
+          () => {
+            executeSearch();
+          }
           //   {
           //   variables: { filter: searchFilter },
           // }
