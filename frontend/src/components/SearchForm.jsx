af33cc70e169b073e6acb5c2b969a8bf54ed0be6
dennisliuu <dennisliuu@gmail.com>
2021-03-31 18:02:24 +0800

    Add show search data

--- frontend/src/components/SearchForm.jsx
+++ frontend/src/components/SearchForm.jsx
@@ -1,28 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ShowResult from "./ShowResult";
 
-const executeSearch = async (req) => {
-  console.log(req.Name);
-  await fetch("http://127.0.0.1:5000/api", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      School: req.School,
-      Name: req.Name,
-    }),
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-      return result;
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-};
 const SearchForm = ({ schools }) => {
   const [professor, setProfessor] = useState([]);
   const [search, setSearch] = useState({
@@ -39,7 +17,15 @@ const SearchForm = ({ schools }) => {
           id={school}
           type="radio"
           name="school_radio"
-          value={school == 'NCTU' ? '1' : school == 'NCU' ? '2': school == 'NTHU' ? '3' : '4'}
+          value={
+            school == "NCTU"
+              ? "1"
+              : school == "NCU"
+              ? "2"
+              : school == "NTHU"
+              ? "3"
+              : "4"
+          }
           onChange={(e) =>
             setSearch({ Name: search.Name, School: e.currentTarget.value })
           }
@@ -68,10 +54,33 @@ const SearchForm = ({ schools }) => {
       <button
         className="button is-plain is-primary is-sm is-margin-top-xxl"
         type="button"
-        onClick={() => {
-          executeSearch({
-            Name: search.Name,
-            School: search.School,
+        onClick={async () => {
+          let res = await fetch("http://127.0.0.1:5000/api", {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              School: search.School,
+              Name: search.Name,
+            }),
+          })
+            .then((response) => response.json())
+            .then((result) => {
+              return result;
+            })
+            .catch((error) => {
+              throw error;
+            });
+          setProfessor({
+            Name: res.Name,
+            School: res.School,
+            Y1: res.Y1,
+            Y2: res.Y2,
+            Y2_3: res.Y2_3,
+            Y3_4: res.Y3_4,
+            Y4_beyond: res.Y4_beyond,
           });
         }}
       >
--- frontend/src/components/ShowResult.jsx
+++ frontend/src/components/ShowResult.jsx
@@ -4,7 +4,23 @@ const ShowResult = ({ professor }) => {
   return (
     <div className="card is-floating is-zoom is-outline">
       <div className="box is-padding-md">
-        {professor.map((val) => {
+        {Object.keys(professor).map((index) => {
+          return (
+            <div key={index}>
+              <h1>Name: {professor["Name"]} </h1>
+              <h3>School: {professor["School"]}</h3>
+              <ul className="list is-square">
+                <li className="item">一年畢業的天才：{professor["Y1"]}</li>
+                <li className="item">兩年準時畢業：{professor["Y2"]}</li>
+                <li className="item">兩到三年畢業：{professor["Y2_3"]}</li>
+                <li className="item">三到四年畢業：{professor["Y3_4"]}</li>
+                <li className="item">四年以上畢業：{professor["Y4_beyond"]}</li>
+              </ul>
+            </div>
+          );
+        })}
+
+        {/* {professor.map((val) => {
           return (
             <div key={val.id}>
               <h1>Name: {val.Name} </h1>
@@ -18,7 +34,7 @@ const ShowResult = ({ professor }) => {
               </ul>
             </div>
           );
-        })}
+        })} */}
       </div>
     </div>
   );
